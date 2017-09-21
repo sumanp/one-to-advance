93a39957a1bc6c27ef9feb88c5731cb63d6467e5
Suman Puri <sumanpuri55@gmail.com>
2017-09-21 14:39:56 +0530

    Fix binary search

--- algorithms/scripts/binary_search.rb
+++ algorithms/scripts/binary_search.rb
@@ -1,23 +1,28 @@
-# Binary search only works when your list is in sorted order. For example, the names in a phone book are sorted in alphabetical order, so you can use binary search to look for a name.
+# Binary search only works when your list is in sorted order. 
+# For example, the names in a phone book are sorted in alphabetical order, so you can use binary search to look for a name.
+
 def binary_search(list, item)
   low = 0
   high = list.length - 1
+  steps = 0
 
   while low <= high
-    mid = (low + high)
+    steps += 1
+
+    mid = (low + high) / 2
     guess = list[mid]
     if guess == item
-      return mid
+      return "Item #{mid} found in #{steps} #{steps == 1 ? 'step' : 'steps'}"
     elsif guess > item
       high = mid - 1
     else
       low = mid + 1
     end
-  end
-  return "Nothing found"
+  end 
+   return "Nothing found"
 end
 
-my_list = [1,3,4,7,9]
+my_list = [1,3,4,7,9,5,8,10,11,12,13]
 
-puts binary_search(my_list, 7)
-puts binary_search(my_list, 10)
+puts binary_search(my_list, 12)
+puts binary_search(my_list, 5)
